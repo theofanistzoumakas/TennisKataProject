9a92f3c1b36c69dc450800fcfa75283c424301fd
theofanistzoumakas <theftzoumi@gmail.com>
2026-04-03 12:39:22 +0300

    feat: handle Player 1 Wins scenario for 4-0

--- TennisKataProject/Services/TennisService.cs
+++ TennisKataProject/Services/TennisService.cs
@@ -14,6 +14,7 @@
             if ((p1Points == 3 && p2Points == 3) || p1Points == 4 && p2Points == 4) return "Deuce";
             if (p1Points == 4 && p2Points == 3) return "Advantage for Player 1";
             if (p1Points == 3 && p2Points == 4) return "Advantage for Player 2";
+            if (p1Points == 4 && p2Points == 0) return "Player 1 Wins";
 
             return $"{p1Points}-{p2Points}";
         }
