f1d3e6514884051f468109305e8a8d1a41a127e6
theofanistzoumakas <theftzoumi@gmail.com>
2026-04-03 11:38:28 +0300

    test: add failing BadRequest for finished game in PlayerScores method

--- TennisKata.Tests/TennisControllerTest.cs
+++ TennisKata.Tests/TennisControllerTest.cs
@@ -102,5 +102,38 @@ namespace TennisKata.Tests
             // Assert
             Assert.Equal("Player not found.", badRequestResult.Value);
         }
+
+        [Fact]
+        public async Task PlayerScores_ShouldReturnBadRequest_WhenGameIsFinished()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            using var context = new AppDbContext(options);
+
+            var controller = new TennisController(context, new TennisService());
+
+            var game = new TennisGame
+            {
+                Id = 2,
+                Player1Points = 4,
+                Player2Points = 0,
+                isFinished = true,
+                CurrentScoreText = "Player 1 Wins"
+            };
+
+            context.Add(game);
+
+            await context.SaveChangesAsync();
+
+            // Act
+            var result = await controller.PlayerScores(2, "Player1");
+
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+
+            // Assert
+            Assert.Equal("The game is finished.", badRequestResult.Value);
+        }
     }
 }
