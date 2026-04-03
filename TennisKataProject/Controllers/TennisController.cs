4647abd5e283939c4ec52c5c98994eeb4a201b47
theofanistzoumakas <theftzoumi@gmail.com>
2026-04-03 14:25:02 +0300

    fix: update IsFinished to true when a player wins (GREEN)

--- TennisKataProject/Controllers/TennisController.cs
+++ TennisKataProject/Controllers/TennisController.cs
@@ -49,6 +49,8 @@ namespace TennisKataProject.Controllers
             else if (player.Equals("Player2")) game.Player2Points++;
             else return BadRequest("Player not found.");
 
+            if (game.CurrentScoreText.Equals("Player 1 Wins") || game.CurrentScoreText.Equals("Player 2 Wins")) game.isFinished = true;
+
             game.CurrentScoreText = _tennisService.CalculateScore(game.Player1Points, game.Player2Points);
 
             await _context.SaveChangesAsync();
