17de88209193cba7d62aba46aa9ef47c38f94fd1
theofanistzoumakas <theftzoumi@gmail.com>
2026-04-03 11:49:22 +0300

    feat: add score calculation for Love-All (GREEN)

--- TennisKataProject/Services/TennisService.cs
+++ TennisKataProject/Services/TennisService.cs
@@ -4,6 +4,9 @@
     {
         public string CalculateScore(int p1Points, int p2Points)
         {
+
+            if (p1Points == 0 && p2Points == 0) return "Love-All";
+
             return $"{p1Points}-{p2Points}";
         }
     }
