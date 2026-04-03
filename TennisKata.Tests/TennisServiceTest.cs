f52f10ceb8a34a85e77bed8417a4ca76200c64b9
theofanistzoumakas <theftzoumi@gmail.com>
2026-04-03 12:03:53 +0300

    test: add 40-Love scenario (RED)

--- TennisKata.Tests/TennisServiceTest.cs
+++ TennisKata.Tests/TennisServiceTest.cs
@@ -14,6 +14,7 @@ namespace TennisKata.Tests
         [InlineData(0, 0, "Love-All")]
         [InlineData(1, 0, "15-Love")]
         [InlineData(0, 2, "Love-30")]
+        [InlineData(3, 0, "40-Love")]
         public void CalculateScore_WithVariousPoints_ReturnsCorrectScore(int p1Points, int p2Points, string expectedScore)
         {
             var service = new TennisService();
