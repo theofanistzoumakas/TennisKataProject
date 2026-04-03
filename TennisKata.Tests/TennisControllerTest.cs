75971355e4126d6b991883b461391630e64a4cdf
theofanistzoumakas <theftzoumi@gmail.com>
2026-04-03 10:19:18 +0300

    test: add failing test for game creation (RED)

--- TennisKata.Tests/TennisControllerTest.cs
+++ TennisKata.Tests/TennisControllerTest.cs
@@ -1,10 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using TennisKataProject.Controllers;
+using TennisKataProject.Data;
+using TennisKataProject.Services;
+
 namespace TennisKata.Tests
 {
     public class TennisControllerTest
     {
         [Fact]
-        public void Test1()
+        public async Task Post_ShouldCreateGame_AndReturnCreatedAtAction()
         {
+            // Arrange
+            var options = new DbContextOptionsBuilder<AppDbContext>().UseInMemoryDatabase(databaseName: "PostTestDb")
+                .Options;
+
+            using var context = new AppDbContext(options);
+
+            var controller = new TennisController(context, new TennisService());
+
+            // Act
+            var result = await controller.StartGame();
+
+            // Assert
+            var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result);
+            Assert.Equal(201, createdAtActionResult.StatusCode);
+
+            Assert.Equal(1, await context.TennisGames.CountAsync());
 
         }
     }
--- TennisKataProject/Controllers/TennisController.cs
+++ TennisKataProject/Controllers/TennisController.cs
@@ -1,6 +1,24 @@
-﻿namespace TennisKataProject.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using TennisKataProject.Data;
+using TennisKataProject.Services;
+
+namespace TennisKataProject.Controllers
 {
-    public class TennisController
+    public class TennisController : ControllerBase
     {
+        private readonly AppDbContext _context;
+        private readonly TennisService _tennisService;
+
+        public TennisController(AppDbContext context, TennisService tennisService)
+        {
+            _context = context;
+            _tennisService = tennisService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> StartGame()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
